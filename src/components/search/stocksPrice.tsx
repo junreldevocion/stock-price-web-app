f5f0d591bde4dfac9541e9f3e3c71cfee05fd58d
Junrel Devocion <junrel.devocion.-nd@disney.com>
2024-08-15 11:46:03 +0800

    fix price display in mobile

--- src/components/search/stocksPrice.tsx
+++ src/components/search/stocksPrice.tsx
@@ -3,7 +3,7 @@ import { StockPricesType } from '@/types/global';
 export function StocksPrice({ title, value }: StockPricesType) {
   return (
     <div
-      className="mb-4 grid grid-cols-[1fr_150px] items-center pb-4 last:mb-0 last:pb-0"
+      className="mb-4 grid-cols-[1fr_50px] grid md:grid-cols-[1fr_150px] items-center pb-4 last:mb-0 last:pb-0"
     >
       <span>{title}</span>
       <div className="space-y-1">
